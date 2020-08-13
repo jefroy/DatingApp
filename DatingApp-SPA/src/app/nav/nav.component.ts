61591a8113f33c80dd3a1fca831eb825421b9ca2
Ajay <ajay_sieunarine@hotmail.com>
2020-08-13 11:24:48 -0400

    auth service and nav bar

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {}; // store username and pw from the html
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  // login method
+  login(){
+    // console.log(this.model); // make sure everything is hooked up
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged in successfully');
+    }, error => {
+      console.log('Failed to login');
+    });
+  }
+
+}
