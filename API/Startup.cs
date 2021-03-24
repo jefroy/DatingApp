c7e3835273c00ca59727cf1a1f3591c9912d9227
Ajay Sieunarine <38733594+jefroy@users.noreply.github.com>
2021-03-24 04:08:17 -0400

    make connection string
    install design package for dbmigrations
    
    do initial db migrate using:
    - $ dotnet ef migrations add InitialCreate -o Data/Migrations

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,20 +18,20 @@ namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            _config = config; // private read only variable from param
         }
-
-        public IConfiguration Configuration { get; }
-
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             // my stuff
             services.AddDbContext<DataContext>(options =>
             {
-                options.UseSqlite("Connection String");
+                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             
             services.AddControllers();
