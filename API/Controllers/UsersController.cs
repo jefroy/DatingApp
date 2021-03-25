7ac9073965522e70e01a76d9456d5f9e0564216f
Ajay Sieunarine <38733594+jefroy@users.noreply.github.com>
2021-03-25 10:12:33 -0400

    [controller] Make users controller async

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -19,14 +21,14 @@ namespace API.Controllers
         
         // endpoints
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            return _context.Users.ToList();
+            return await _context.Users.ToListAsync();
         }
         [HttpGet("{id}")] // id = user parameter
-        public ActionResult<AppUser> GetUser(int id)
+        public async Task<AppUser> GetUser(int id)
         {
-            return _context.Users.Find(id);
+            return await _context.Users.FindAsync(id);
         }
     }
 }
