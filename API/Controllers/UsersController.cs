2aa9d3f3ab92463bc375d17c1c5a3267ad355e36
Ajay Sieunarine <38733594+jefroy@users.noreply.github.com>
2021-03-25 10:07:13 -0400

    [controller] setup UsersController
    - tested results in postman
    
    - https://localhost:5001/api/users
    
    - https://localhost:5001/api/users/2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // go to api/users
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        
+        // endpoints
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+        [HttpGet("{id}")] // id = user parameter
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.Users.Find(id);
+        }
+    }
+}
