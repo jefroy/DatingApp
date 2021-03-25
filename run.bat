f135062ba07d960fd03c5cc20124b20271d4d91f
Ajay Sieunarine <38733594+jefroy@users.noreply.github.com>
2021-03-25 08:13:11 -0400

    [qol] add db files to gitignore, adjust run script

--- run.bat
+++ run.bat
@@ -1,7 +1,19 @@
+@echo off
+
+start "VSCODE" cmd /c "code . & exit /b"
+
 cd API
-start cmd.exe /k "dotnet watch run"
+start "dotnet watch run" cmd /c "dotnet watch run & exit /b"
+start "RIDER" cmd /c "rider.bat . & exit /b"
 
 cd ..
 
-cd DatingApp-SPA
-start cmd.exe /k "ng serve"
+rem cd DatingApp-SPA
+rem start "ng serve" cmd /c "ng serve & exit /b"
+
+rem closeup
+TASKKILL /IM Code.exe
+TASKKILL /IM rider64.exe
+
+exit /b
+
