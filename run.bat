5e1372ee94aab70cc8a44d07bb27af050fb14f39
Ajay Sieunarine <38733594+jefroy@users.noreply.github.com>
2021-03-29 04:09:47 -0400

    [api] [auth] Setup JWT service
    Resolves #19
    
    - [qol] adjust `run.bat`
    
    - implement token interface in /Interfaces
    
    - implement token service in /Services (uses interface)

--- run.bat
+++ run.bat
@@ -3,14 +3,16 @@
 start "VSCODE" cmd /c "code . & exit /b"
 
 cd API
-start "dotnet watch run" cmd /c "dotnet watch run & exit /b"
+@REM start "dotnet watch run" cmd /c "dotnet watch run & exit /b"
 start "RIDER" cmd /c "rider.bat . & exit /b"
 
 cd ..
 
 cd client
-start "ng serve" cmd /c "ng serve & exit /b"
+@REM start "ng serve" cmd /c "ng serve & exit /b"
 start "WEBSTORM" cmd /c "webstorm.bat . & exit /b"
+
+cd ..
 
 rem closeup
 TASKKILL /IM Code.exe
