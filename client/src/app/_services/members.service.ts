c75584d7a4b2aff42dae250d7020be53dfa17bdf
Ajay Sieunarine <38733594+jefroy@users.noreply.github.com>
2021-05-05 06:30:30 -0400

    [front] use interceptor to send token
    Resolves #97

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,14 +1,8 @@
 import { Injectable } from '@angular/core';
 import {environment} from '../../environments/environment';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Member} from '../_models/member';
 import {Observable} from 'rxjs';
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
-  })
-};
 
 @Injectable({
   providedIn: 'root'
@@ -19,11 +13,11 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers(): Observable<Member[]>{
-    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 
   getMember(username: string): Observable<Member>{
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, httpOptions);
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
 }
